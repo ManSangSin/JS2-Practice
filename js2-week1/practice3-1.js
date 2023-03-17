e869be5072b789c6eda3e40b3457585138923608
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-03-17 18:49:18 +0000

    completed practice3-1 and practice3-2
    watched "nested objects" video
    completed JS2 week1 practice: 3-1 and 3-2

---
+++ js2-week1/practice3-1.js
@@ -0,0 +1,12 @@
+uk = {
+  name: "The United Kingdom of Great Britain and Northern Ireland",
+  shortName: "The UK",
+  capitalCity: {
+    name: "London",
+    population: 9648000,
+  },
+};
+
+console.log(
+  `The capital of ${uk.shortName} is ${uk.capitalCity.name}, ${uk.capitalCity.name} has a population of ${uk.capitalCity.population}`
+);
