67912e432e7e7c5f4fd673f3dad240b8ed989b94
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-03-17 13:40:27 +0000

    completed practice2-1
    watched video: "mutating objects"
    completed JS2 week1 practice: Add object field task

---
+++ js2-week1/practice2-1.js
@@ -0,0 +1,14 @@
+let stock = {
+  name: "aapl",
+  lastClosingPrice: 172.99,
+  lastFiveDaysClosingPrices: [179.99, 180.33, 176.28, 175.64, 172.99],
+};
+
+// write code here to add the changeInLastFiveDays field to stock
+
+(stock.changeInLastFiveDays =
+  stock.lastFiveDaysClosingPrices[4] - stock.lastFiveDaysClosingPrices[0]),
+  console.log(
+    "In the last 5 days, the stock changed by",
+    stock.changeInLastFiveDays
+  );
